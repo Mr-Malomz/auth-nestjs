572cc4378bf63db619524c69ad214339ddd1f8a1
Demola Malomo <malomo.alaba@yahoo.com>
2020-08-30 09:13:27 +0100

    Signup dto and validation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,18 @@
-import { Repository, EntityRepository } from "typeorm";
-import { User } from "./user.entity";
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthSignUpDto } from './dto/auth-signup.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async signUp(authSignUpDto: AuthSignUpDto) {
+    const { email, password, firstname, lastname } = authSignUpDto;
 
+    const user = new User();
+    user.email = email;
+    user.password = password;
+    user.firstname = firstname;
+    user.lastname = lastname;
+
+    await user.save()
+  }
 }
