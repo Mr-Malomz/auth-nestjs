0d354fdb04ab581d5176bd6f3aac7f0756409f4c
Demola Malomo <malomo.alaba@yahoo.com>
2020-09-14 18:32:45 +0100

    jwt, passport and signIn service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthSignUpDto } from './dto/auth-signup.dto';
+import { AuthSignInDto } from './dto/auth-signin.dto';
+import { UserJwtResponse } from './interface/user-jwt-response.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,10 @@ export class AuthController {
   @Post('/signup')
   async signUp(@Body() authSignUpDto: AuthSignUpDto): Promise<string> {
     return this.authService.signUp(authSignUpDto);
+  }
+
+  @Post('/signin')
+  async signIn(@Body() authSignInDto: AuthSignInDto): Promise<UserJwtResponse> {
+    return this.authService.signIn(authSignInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,16 +4,32 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthSignUpDto } from './dto/auth-signup.dto';
 import { AuthSignInDto } from './dto/auth-signin.dto';
 import { UserJwtResponse } from './interface/user-jwt-response.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authSignUpDto: AuthSignUpDto): Promise<string> {
     return this.userRepository.signUp(authSignUpDto);
   }
 
+  async signIn(authSignInDto: AuthSignInDto): Promise<UserJwtResponse> {
+    const userResult = await this.userRepository.signIn(authSignInDto);
+
+    if (!userResult) {
+      throw new UnauthorizedException('Invalid Credential!');
+    }
+
+    const payload = { userResult };
+    const accessToken = await this.jwtService.sign(payload);
+
+    const signInResponse: UserJwtResponse = { user: userResult, accessToken };
+
+    return signInResponse;
+  }
 }
